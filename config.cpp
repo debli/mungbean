7d90d9cecf58d8a54e8e32497dae2db104df0721
debli <deblicn@gmail.com>
2015-12-04 14:45:59 +0800

    modified:   config.cpp modified:   config.h

--- config.cpp
+++ config.cpp
@@ -1,4 +1,18 @@
 #include "config.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
+
+int Config::load(const char* file)
+{
+    ofstream cfgFile;
+    cfgFile.open (file);
+    cfgFile << "Writing this to a file.\n";
+    cfgFile.close();
+    return 0;
+}
 
 Php::Value Config::inc(Php::Parameters &params)
 { 
--- config.h
+++ config.h
@@ -1,12 +1,27 @@
 #include <phpcpp.h>
+#include <iostream>
+#include <fstream>
+#include <string>
+
+using namespace std;
 
 class Config: public Php::Base
 {
     private:
         int _val = 0;
+        /* load configure file */
+        int load(const char* file);
 
     public:
-        Config() {}
+        Config(Php::Parameters &params)
+        {
+            if (params.size() == 0)
+                return;
+            cout << params[0] << endl;
+            this->load((const char *)params[0]);
+            
+        }
+
         virtual ~Config() {}
         
         Php::Value inc(Php::Parameters &params);
